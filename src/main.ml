a5c93e08dda13eda2ec445c55d0db2c723bb54be
sltcestloic <root@sltcestloic.dev>
2024-07-29 13:28:00 +0200

    test 2 array  (keys et moves)

--- src/main.ml
+++ src/main.ml
@@ -1,2 +1,17 @@
+include Parser
+
 let () =
-  print_endline "Hello, World!"
+  if Array.length Sys.argv != 1 then (
+    print_string "Usage: ./";
+    print_string Sys.argv.(0);
+    print_endline " [file]";
+  )
+  else (
+    let filename = Sys.argv.(1) in
+    if not (Sys.file_exists filename) then (
+      print_endline "File does not exist.";
+      exit 1
+    );
+    let in_channel = open_in filename in
+    Parser.parse in_channel;
+  )
---
+++ src/parser.ml
@@ -0,0 +1,28 @@
+type automaton = Types.automaton
+
+let split_keys_combos lines =
+  let rec aux keys combos is_keys is_combos = function
+    | [] -> (List.rev keys, List.rev combos)
+    | "#Keys" :: rest -> aux keys combos true false rest
+    | "#Combos" :: rest -> aux keys combos false true rest
+    | line :: rest when is_keys -> aux (line :: keys) combos true false rest
+    | line :: rest when is_combos -> aux keys (line :: combos) false true rest
+    | _ :: rest -> aux keys combos is_keys is_combos rest
+  in
+  aux [] [] false false lines
+
+let rec read_lines ic lines =
+  try
+    let line = input_line ic in
+    read_lines ic (line :: lines)
+  with End_of_file ->
+    close_in ic;
+    List.rev lines
+
+let parse ic =
+  try
+    let lines = read_lines ic [] in
+    let keys, combos = split_keys_combos lines in
+  with e ->
+    close_in_noerr ic;
+    raise e
